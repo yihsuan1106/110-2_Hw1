b45b1e6639943ad8369020af95a3be738cde9a21
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2022-04-16 18:40:40 +0800

    Update and rename f to f.py

--- f
+++
@@ -1,6 +0,0 @@
-x = int(input())
-while x > 0:
-    for i in range(x):
-        print("*", end="")
-    print()
-    x-=1
---
+++ f.py
@@ -0,0 +1,13 @@
+x = int(input())
+while x > 0:
+    for i in range(x):
+        print("*", end="")
+    print()
+    x-=1
+
+#n = int(input())
+#for i in range(n):
+#    a = n-i
+#    for j in range(a):
+#        print("*",end="")
+#    print()
