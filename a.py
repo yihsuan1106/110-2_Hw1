9d98dd120398dc8ea427354d6472926642fc94d8
yihsuan1106 <97450167+yihsuan1106@users.noreply.github.com>
2022-04-16 16:28:10 +0800

    Update and rename a to a.py

--- a.py
+++ a.py
@@ -3,6 +3,7 @@ a, b = map(int, input().split())
 
 hight.append(list(map(int, input().rstrip().split()))) 
 hight[0].sort()
+#hight[0] = sorted(hight[0])
 
 for i in range(b):
     num = int(input())
